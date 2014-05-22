3a4ecbf8e949c66e6ac57d6f73d3100de6e020fe
gaethug <cbak@naver.com>
2014-05-23 01:00:44 +0900

    목록, 메인 등 많이

--- client/views/posts/list.js
+++ client/views/posts/list.js
@@ -6,12 +6,26 @@ Template.postsList.helpers({
     return Posts.find({}, {sort: {submitted: -1}});
   }
 });
-Template.postStrip.stripImage = function(){
-
-  var imgsrc = "";
+Template.postStrip.stripTag = function(){
+  var stripTag = {
+    img:"",
+    text:""
+  };
+  var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi,
+    allowed = ((("" || "") + "").toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join(''),
+    commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
   if(this.content){
-    imgsrc = (this.content.match(/<img[^>]*src=[\"']?([^>\"']+)[\"']?[^>]*>/g)||[]);
+    stripTag.img = (this.content.match(/<img[^>]*src=[\"']?([^>\"']+)[\"']?[^>]*>/g)||[]);
+    stripTag.text = this.content.replace(/&nbsp;/g,'').replace(commentsAndPhpTags, '').replace(tags, function ($0, $1){return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';});
+
   }
-  console.log(imgsrc);
-  return imgsrc;
+  return stripTag;
 };
+Template.postStrip.rendered = function() {
+  $('.content-preview').dotdotdot({
+    watch:true
+  });
+  $('.content-title').dotdotdot({
+    watch:true
+  });
+}
