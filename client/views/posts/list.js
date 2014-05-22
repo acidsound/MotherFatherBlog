bf78acab29727e014681db48df85a4fd4519ea4c
gaethug <cbak@naver.com>
2014-05-22 18:50:44 +0900

    목록

--- client/views/posts/list.js
+++ client/views/posts/list.js
@@ -1,5 +1,17 @@
 Template.postsList.helpers({
   posts: function() {
+
+    //var imgSrcs = (post.content.match(/<img[^>]*src=[\"']?([^>\"']+)[\"']?[^>]*>/g)||[]);
+
     return Posts.find({}, {sort: {submitted: -1}});
   }
 });
+Template.postStrip.stripImage = function(){
+
+  var imgsrc = "";
+  if(this.content){
+    imgsrc = (this.content.match(/<img[^>]*src=[\"']?([^>\"']+)[\"']?[^>]*>/g)||[]);
+  }
+  console.log(imgsrc);
+  return imgsrc;
+};
