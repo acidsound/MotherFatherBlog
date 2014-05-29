6644a0f51ff7a42f37594c30bbecf010397ca79c
gaethug <cbak@naver.com>
2014-05-29 17:15:23 +0900

    이미지 추가 개션

--- client/views/categories/popular.js
+++ client/views/categories/popular.js
@@ -9,7 +9,12 @@ Template.popularCategories.helpers({
   popularList: function() {
     //-_- 뭐냐 category.postIds.length 로 sort해야 하는데
     //aggregate는 안되는거 같고
-    var cateArray = Categories.find({}).fetch();
-    return cateArray.sort(function(a, b){return b.postIds.length - a.postIds.length}).slice(0,10);
+    var cateArray = Categories.find().fetch();
+    if(!!cateArray){
+      return cateArray.sort(function(a, b){return b.postIds.length - a.postIds.length}).slice(0,10);
+    }else{
+      return [];
+    }
+
   }
 });
