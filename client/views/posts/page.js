2dea5838050e8ce47aec4abd465a8534a3183dcf
gaethug <cbak@naver.com>
2014-06-01 16:13:50 +0900

    이미지 첨부 팝업을 첨부 팝업으로 확장 및 유투브 동영상 링크 올리면 보여질때 유투브로 파싱하도록

--- client/views/posts/page.js
+++ client/views/posts/page.js
@@ -51,8 +51,9 @@ Template.postItem.events({
 Template.postItem.rendered = function(){
   if (!this.rendered){
     var youtubeElem = $('.content-wrapper').find("img[alt='youtube']");
+    var youtubeId =  youtubeElem.name;
     if(youtubeElem && youtubeElem.length > 0){
-      $(youtubeElem).replaceWith('<iframe width=\"480\" height=\"300\" src=\"http://www.youtube.com/embed/'+ youtubeElem.name + '\" frameborder=\"0\"></iframe>');
+      $(youtubeElem).replaceWith('<iframe width=\"400\" height=\"225\" src=\"http://www.youtube.com/embed/'+youtubeId+ '\" frameborder=\"0\"></iframe>');
 
     }
   }
