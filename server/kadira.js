8612a16852e874ff0c2b9e12c6b662a1c19f66ae
gaethug <cbak@naver.com>
2014-06-05 09:41:23 +0900

    카디라

---
+++ server/kadira.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: hoho
+ * Date: 2014. 6. 5.
+ * Time: 오전 9:35
+ * To change this template use File | Settings | File Templates.
+ */
+Kadira.connect('KM5qoozaxzYjH7o38', '2fb8b9df-4dab-4cc1-a81e-dddee142e14a')
