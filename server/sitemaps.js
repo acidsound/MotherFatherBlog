edccb3548301ef648ce51c33a8624f62230db348
gaethug <cbak@naver.com>
2014-06-06 11:16:58 +0900

    google webmaster 용 사이트 맵 추가 /posts_sitemap.xml

---
+++ server/sitemaps.js
@@ -0,0 +1,17 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: hoho
+ * Date: 2014. 6. 6.
+ * Time: 오전 11:11
+ * To change this template use File | Settings | File Templates.
+ */
+sitemaps.add('/posts_sitemap.xml', function() {
+  var out = [], pages = Posts.find().fetch();
+  _.each(pages, function(page) {
+    out.push({
+      page: '/posts/'+page._id,
+      lastmod: page.submitted
+    });
+  });
+  return out;
+});
