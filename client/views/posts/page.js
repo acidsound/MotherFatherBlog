882710fe6936418bc80c8a8ef81670bf653db618
hohoman <cbak@naver.com>
2014-07-08 22:16:30 +0900

    Update page.js

--- client/views/posts/page.js
+++ client/views/posts/page.js
@@ -55,8 +55,8 @@ Template.postItem.rendered = function(){
       var youtubeId =  $(elem).attr("name");
       //width=\"400\" height=\"225\"
       //
-      //var contentWidth = $('#postItem .content-wrapper').width();
-      $(elem).replaceWith("<iframe  style=\"max-width:100%; \" src=\"http://www.youtube.com/embed/"+youtubeId+ "\" frameborder=0></iframe>");
+      var contentWidth = $('#postItem .content-wrapper').width();
+      $(elem).replaceWith("<iframe width='"+contentWidth+"'  style=\"max-width:100%; \" src=\"http://www.youtube.com/embed/"+youtubeId+ "\" frameborder=0></iframe>");
 
     });
 
