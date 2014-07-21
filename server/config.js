27390da13f9f2bcb906368cca7b69d3e1a426ef5
gaethug <cbak@naver.com>
2014-07-21 10:49:24 +0900

    아놔 병신 indexOf 가지고 삽질했었네. 정신 차려 인마
    underdogg.co.kr -> www.underdogg.co.kr 로 리다이렉팅 해서 URL이 하나인양 좀 해보자 시박 근데 잘 안됨

--- server/config.js
+++ server/config.js
@@ -15,26 +15,18 @@ Cloudinary.config({
 // Redirect to a random URL, better handled client-side
 //underdogg.co.kr => www.underdogg.co.kr로 리다이렉팅
 WebApp.connectHandlers.use(function(req, res, next) {
-  /*    if (!~req.headers.host.indexOf('localhost')) {
-
-   }else{
-   //로컬호스트 왜이러지 -_ ㅠ
-   next();
-   }*/
-  /*res.writeHead(301, {
-   'Location': 'http://underdogg.co.kr' +req.originalUrl
-   });
-   res.end();*/
-  console.log(req.headers.host+"  : "+req.headers.host.indexOf('www.underdogg.co.kr'));
-    if (req.headers.host.indexOf('www.underdogg.co.kr') >= 0) {
-      console.log("영보다 큼");
-      next();
+  //아니 씨발 indexOf 리턴값 때문에 개 삽질한거였어???
+  if(req.headers.host.indexOf('localhost') >= 0){
+    next();
   }else{
-      console.log("영보다 자금 ");
-    res.writeHead(307, {
-      'Location': 'http://www.underdogg.co.kr' +req.url
-    });
-    res.end();
-
+    if(req.headers.host.indexOf('www.underdogg.co.kr') >= 0) {
+      next();
+    }else{
+      res.writeHead(307, {
+        'Location': 'http://www.underdogg.co.kr' +req.url
+      });
+      res.end();
+    }
   }
+
 });
