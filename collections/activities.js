ea8d3523efd389d66654197afe1e3644ffcda291
gaethug <cbak@naver.com>
2014-07-25 11:30:40 +0900

    레이아웃 수정

--- collections/activities.js
+++ collections/activities.js
@@ -9,7 +9,7 @@ Activities = new Meteor.Collection('activities');
 
 ACTIVITY_TYPES={
   "create_post":{type:"post", method:"create",  message : "새로운 포스트", icon:"fa fa-file-image-o"},
-  "update_post":{type:"post", method:"update",  message : "포스트 수정", icon:"fa fa-pencil"},
+  "update_post":{type:"post", method:"update",  message : "포스트 수정", icon:"fa fa-wrench"},
   "vote_post":{type:"post", method:"vote",  message : "엠지!", icon:"fa fa-thumbs-o-up"},
   "create_comment":{type:"comment", method:"create",message : "새로운 댓글", icon:"fa fa-comment-o"}
 };
