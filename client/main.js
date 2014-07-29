ee0121057106ec85a331b67cacba44e798d79a8b
gaethug <cbak@naver.com>
2014-07-29 15:52:51 +0900

    레이아웃 수장

--- client/main.js
+++ client/main.js
@@ -40,7 +40,7 @@ Meteor.startup(function() {
       meta : ['fragment','viewport', 'mobile-web-app-capable', 'apple-mobile-web-app-capable', 'apple-mobile-web-app-status-bar-style']
     },
     meta: {
-      'description': '요리사가 자기 저녁밥 만들어 먹고 목수가 자기 집 신발장 짜고 치과 의사가 자기 이빨 신경 치료 하듯 나도 내 웹 사이트는 직접...'
+      'description': 'Linux linaro-server 3.0.36+ #2 SMP PREEMPT Mon Jan 27 15:07:00 ICT 2014 armv7l armv7l armv7l GNU/Linux - 방구석 자비스'
     },
     og: {
       'image': 'http://www.underdogg.co.kr/images/logo.png'
