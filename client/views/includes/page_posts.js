7ac8aaf8d269f30a9efae27d79ddc0b38a4cacd7
gaethug <cbak@naver.com>
2014-07-30 18:11:29 +0900

    클라이언트 사이드 페이징 개 짜증나네

---
+++ client/views/includes/page_posts.js
@@ -0,0 +1,54 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: hoho
+ * Date: 2014. 5. 29.
+ * Time: 오전 10:00
+ * To change this template use File | Settings | File Templates.
+ */
+Template.page_posts.created = function(){
+  Session.set('perPage', 5);
+  Session.set('pageNumber',0 );
+}
+Template.page_posts.helpers({
+  pageList: function() {
+    return Posts.find({},{sort: {submitted: -1}, limit:  Session.get('perPage'), skip: Session.get('perPage')* Session.get('pageNumber')}).fetch();
+  },
+  totalCount : function(){
+    return Posts.find().count();
+  },
+  countList : function(){
+    var countList = [];
+    for(var i = 0, l = Posts.find().count() / Session.get('perPage'); i < l ; i++){
+      var item = {};
+      if(Session.get('pageNumber') == i){
+        item.activeClass = "active";
+      }else {
+        item.activeClass = "";
+      }
+      item.index = i;
+      item.dispIndex = i+1;
+      countList.push(item);
+    }
+    return countList;
+  }
+});
+Template.page_posts.events({
+  'click .previous': function (evt, tmpl) {
+    evt.preventDefault();
+    console.log(tmpl);
+    var count = Session.get('pageNumber') - 1;
+    if(count > 0){
+      Session.set('pageNumber', count);
+      //$(tmpl.firstNode).find('.previous').addClass("active");
+    }
+  },
+  'click .next': function (evt, tmpl) {
+    evt.preventDefault();
+    var count = Session.get('pageNumber') + 1;
+    Session.set('pageNumber', count);
+  },
+  'click .page-item': function (evt, tmpl) {
+    evt.preventDefault();
+    Session.set('pageNumber', $(evt.currentTarget).attr("data-page-number"));
+  }
+});
